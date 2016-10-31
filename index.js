fb8fc23946c9792d95d2d9795b31796458d1ddac
James McElwain <James.McElwain@olson.com>
2016-10-31 13:55:35 -0500

    guard against undefined stats objects resulting from things like swap files

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ module.exports = function(dir, /* optional */ pattern, /* optional */ excludePat
     async.map(files, fs.stat, function(err, results) {
       // Figure out which ones are directores that we need to recurse into
       files.forEach(function(file, i) {
-        if (results[i].isDirectory()) {
+        if (results[i] && results[i].isDirectory()) {
           dirs.push(file)
         } else {
           outputFiles.push(file)
